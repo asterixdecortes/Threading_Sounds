3f0c528611ede6fc4dbf865b9af6af2806b01251
asterixdecortes <asterixensucasa@gmail.com>
2025-07-22 23:53:19 +0200

    Working version but with only Songs table, working on new version with the new ERD schema

--- Threading-Sounds/src/main/java/com/threadingsounds/Threading_Sounds/MainController.java
+++ Threading-Sounds/src/main/java/com/threadingsounds/Threading_Sounds/MainController.java
@@ -2,13 +2,16 @@ package com.threadingsounds.Threading_Sounds;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +48,21 @@ public class MainController {
         return ResponseEntity.ok(foundSong);
     }
 
-    public void deleteSong(int id) {
-        //TODO, take from SERVICE
+    @GetMapping
+    public ResponseEntity<List<Song>> getAllSongs() {
+        List<Song> songs = songsService.getAllSongs();
+        return ResponseEntity.ok(songs);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Song> updateSong(@PathVariable("id") Long songId, @RequestBody Song updatedSong) {
+        Song savedSong = songsService.updateSong(songId, updatedSong);
+        return ResponseEntity.ok(savedSong);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteSong(@PathVariable("id") Long songId) {
+        songsService.deleteSong(songId);
+        return ResponseEntity.ok("Song deleted");
     }
 }
--- Threading-Sounds/src/main/java/com/threadingsounds/Threading_Sounds/MainService.java
+++ Threading-Sounds/src/main/java/com/threadingsounds/Threading_Sounds/MainService.java
@@ -1,5 +1,7 @@
 package com.threadingsounds.Threading_Sounds;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.threadingsounds.Threading_Sounds.exceptions.ResourceNotFoundException;
@@ -20,6 +22,35 @@ public class MainService {
     public Song getSongById(Long songId) {
         Song foundSong = songsRepo.findById(songId)
             .orElseThrow(() -> new ResourceNotFoundException("Song with id " + songId + " does not exist"));
-        return (foundSong);
+        return foundSong;
+    }
+
+    public List<Song> getAllSongs(){
+        List<Song> songs = songsRepo.findAll();
+        return songs;
+    } 
+
+    public Song updateSong(Long songId, Song updatedSong) {
+        Song songToEdit = songsRepo.findById(songId)
+            .orElseThrow(() -> new ResourceNotFoundException("Song with id " + songId + " does not exist"));
+        
+        songToEdit.setTitle(updatedSong.getTitle());
+        songToEdit.setAlbum(updatedSong.getAlbum());
+        songToEdit.setArtist(updatedSong.getArtist());
+        songToEdit.setLength(updatedSong.getLength());
+
+        Song savedSong = songsRepo.save(songToEdit);
+
+        return savedSong;
+    }
+
+    public void deleteSong(Long songId) {
+
+        // Currently managing the error with this exception but it can be deleted
+        
+        Song songToDelete = songsRepo.findById(songId)
+            .orElseThrow(() -> new ResourceNotFoundException("Song with id " + songId + " does not exist"));
+        
+        songsRepo.deleteById(songId);
     }
 }
